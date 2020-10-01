0e5258b2ee5693d531b22276db60d5ba4016cf06
Nguyễn Tiến Dũng <james.spears432@gmail.com>
2020-10-01 14:25:53 +0700

    1/10
    Thêm thuộc tính của Household và Fee, chức năng thêm khoản đóng góp trong Household

---
+++ src/main/java/com/nhom25/btl_cnpm/entity/Fee.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moneymanagement;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ * @author James Nguyen
+ */
+public class Fee {
+    private int mId;
+    public String name;
+    public int totalMoney;
+    public int numOfHousehold;
+    public HashMap<Integer, Integer> listOfHousehold; // contains the id of household contributed
+
+    public Fee(int mId, String name, int totalMoney, int numOfHousehold) {
+        this.mId = mId;
+        this.name = name;
+        this.totalMoney = totalMoney;
+        this.numOfHousehold = numOfHousehold;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(int totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    public int getNumOfHousehold() {
+        return numOfHousehold;
+    }
+
+    public void setNumOfHousehold(int numOfHousehold) {
+        this.numOfHousehold = numOfHousehold;
+    }
+
+    public List<Integer> getListOfHousehold() {
+        return listOfHousehold;
+    }
+
+    public void setListOfHousehold(List<Integer> listOfHousehold) {
+        this.listOfHousehold = listOfHousehold;
+    }
+    
+}
--- src/main/java/com/nhom25/btl_cnpm/entity/Household.java
+++ src/main/java/com/nhom25/btl_cnpm/entity/Household.java
@@ -3,14 +3,96 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.nhom25.btl_cnpm.entity;
+package moneymanagement;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  *
- * @author hungn
+ * @author James Nguyen
  */
 public class Household {
-    // viết các thuộc tính
-    // constructor
-    // getter and setter
+    private int mId;
+    public String householder;
+    public int numOfPeople;
+    public int money;
+    public HashMap<Integer, Integer> listOfFee;
+
+    public Household(int mId, String householder, int numOfPeople, int money) {
+        this.mId = mId;
+        this.householder = householder;
+        this.numOfPeople = numOfPeople;
+        this.money = money;
+    } 
+    
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getHouseholder() {
+        return householder;
+    }
+
+    public void setHouseholder(String householder) {
+        this.householder = householder;
+    }
+
+    public int getNumOfPeople() {
+        return numOfPeople;
+    }
+
+    public void setNumOfPeople(int numOfPeople) {
+        this.numOfPeople = numOfPeople;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public List<Integer> getListOfFee() {
+        return listOfFee;
+    }
+
+    public void setListOfFee(List<Integer> listOfFee) {
+        this.listOfFee = listOfFee;
+    }
+    
+    /**
+     * Charge to a fee
+     * @param fee
+     * @param money 
+     */
+    public void addContribute(Fee fee, int money){
+        listOfFee.put(fee.getmId(), money);
+        this.money += money;
+        
+        fee.listOfHousehold.put(this.mId, money);
+        fee.totalMoney += money;                
+    }
+
+    /**
+     * Remove charge to a fee
+     * @param fee 
+     */
+    public boolean removeContribute(Fee fee){
+        if(listOfFee.containsKey(fee.getmId())){
+            int money = listOfFee.get(fee.getmId());
+            listOfFee.remove(fee.getmId());
+            fee.totalMoney -= money;
+            fee.listOfHousehold.remove(mId);
+            return true;
+        }
+        
+        return false;
+                    
+    }
 }
