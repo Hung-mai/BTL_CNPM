788be0911a8d8b7d5b7a1bf5120ab719a848e12e
Hung mai <hungnaruto2511@gmail.com>
2020-10-01 12:47:14 +0700

    tao cau truc thu muc cua project

---
+++ src/main/java/com/nhom25/btl_cnpm/entity/Household.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nhom25.btl_cnpm.entity;
+
+/**
+ *
+ * @author hungn
+ */
+public class Household {
+    
+}
